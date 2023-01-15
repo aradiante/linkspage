cd6d9c48cfad7fd86c4313f7e7955c40ce62cb8d
leonardvsc <leonardoworld@outlook.com>
2023-01-14 23:22:38 -0300

    Alterando a imagem com o tema light

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar_light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
 }
