c1dfedfc8de55e6159b88c0d18ca24b94d23def3
leonardvsc <leonardoworld@outlook.com>
2023-01-14 23:32:03 -0300

    Teste

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar_light.png")
+    img.setAttribute("src", "./assets/avatar.png")
   } else {
     img.setAttribute("src", "./assets/avatar.png")
   }
